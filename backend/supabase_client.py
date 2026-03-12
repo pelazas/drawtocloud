f4acf2b79d4b7c7bc3a566f0893a7ce22ffed495
pelazas <carlospelazas@gmail.com>
2026-03-12 17:24:21 +0100

    change env vars

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -3,5 +3,5 @@ from supabase import create_client, Client
 
 supabase: Client = create_client(
     os.environ["SUPABASE_URL"],
-    os.environ["SUPABASE_SERVICE_KEY"],  # service key for server-side ops
+    os.environ["SUPABASE_SECRET_KEY"],  # service key for server-side ops
 )
